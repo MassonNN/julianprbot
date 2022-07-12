805821b5bc9270c61dc82a317cffe480ac9e1734
Ilya <nestyreff@ya.ru>
2022-07-12 19:47:26 +0300

    добавлены пояснения

--- bot/commands/callback_data_states.py
+++ bot/commands/callback_data_states.py
@@ -2,5 +2,8 @@ from aiogram.dispatcher.filters.callback_data import CallbackData
 
 
 class TestCallbackData(CallbackData, prefix="test"):
+    """
+    Внимание! Фабрики колбеков будут удалены в следующих обновлениях aiogram
+    """
     text: str
     user_id: int
--- bot/middlewares/register_check.py
+++ bot/middlewares/register_check.py
@@ -9,7 +9,15 @@ from bot.db import User
 
 
 class RegisterCheck(BaseMiddleware):
+    """
+    Middleware будет вызываться каждый раз, когда пользователь будет отправлять боту сообщения (или нажимать
+    на кнопку в инлайн-клавиатуре).
+    """
+
     def __init__(self):
+        """
+        Не нужен в нашем случае
+        """
         pass
 
     async def __call__(
@@ -18,23 +26,44 @@ class RegisterCheck(BaseMiddleware):
         event: Union[Message, CallbackQuery],
         data: Dict[str, Any]
     ) -> Any:
+        """ Сама функция для обработки вызова """
+
+        # Получаем менеджер сессий из ключевых аргументов, переданных в start_polling()
         session_maker: sessionmaker = data['session_maker']
+
+        # добавляем синхронный контекст
         async with session_maker() as session:
+
+            # в синхронном режиме запускаем работу с сессией
             async with session.begin():
+
+                # получаем результат выбора ВСЕХ пользователей ГДЕ user_id равен user_id из события (сообщения)
                 result = await session.execute(select(User).where(User.user_id == event.from_user.id))
+
+                # получаем и обрабатываем пользователя
                 user: User = result.one_or_none()
 
                 if user is not None:
+                    # значит пользователь уже зарегистрирован
                     pass
                 else:
+                    # следует зарегистрировать пользователя
+
+                    # создаем экземпляр для нашего пользователя
                     user = User(
                         user_id=event.from_user.id,
                         username=event.from_user.username
                     )
+
+                    # "мержим" его в сессию
                     await session.merge(user)
+
                     if isinstance(event, Message):
                         await event.answer('Ты успешно зарегистрирован(а)!')
                     else:
                         await event.message.answer('Ты успешно зарегистрирован(а)!')
 
+                    # контекстный менеджер session.begin() сам сохранит все изменения!!!
+
+        # передаем управление в хендлер
         return await handler(event, data)
