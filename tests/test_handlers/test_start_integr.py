7daedac25afd4c7314a7c6c8ceb4dc392565f55f
Masson <35528949+MassonNN@users.noreply.github.com>
2023-01-27 11:28:48 +0300

    Update test_start_integr.py

--- tests/test_handlers/test_start_integr.py
+++ tests/test_handlers/test_start_integr.py
@@ -1,6 +1,7 @@
 import pytest
 from aiogram import Dispatcher, Bot
 from aiogram.methods import SendMessage
+from aiogram.dispatcher.event.bases import UNHANDLED
 
 from bot.structures.keyboards import MENU_BOARD
 from utils import get_update, get_message, get_callback_query
@@ -12,6 +13,10 @@ async def test_start_command(dispatcher: Dispatcher, bot: Bot):
     assert isinstance(result, SendMessage)
     assert result.text == 'Меню'
     assert result.reply_markup == MENU_BOARD
+    
+    # Проверяем, что бот ничего не отвечает в ответ на неизвестную команду
+    result = await dispatcher.feed_update(bot=bot, update=get_update(message=get_message(text='/not-start')))
+    assert isinstance(result, UNHANDLED). # UNHANDLED означает "необработанный"
 
 
 @pytest.mark.asyncio
